6ac9c9ce0a45cd1f742d309a5e8e0fec1f0718d6
Dung Tran Anh <min98st1@gmail.com>
2023-03-18 20:14:25 +0700

    Update device driver
    Signed-off-by: Dung Tran Anh <min98st1@gmail.com>

--- device-driver/src/uart_bluetooth.c
+++ device-driver/src/uart_bluetooth.c
@@ -52,6 +52,7 @@ struct uart_ops {
 	/* ... */
 };
 
+// Support timer when uart irq not support/available
 struct nordic_uart {
 	struct uart_port port;
 	struct timer_list tmr;
@@ -116,17 +117,75 @@ static void nordic_uart_set_termios(struct uart_port *port,
 
 static void nordic_uart_rx_chars(struct uart_port *port)
 {
+	struct tty_port *tty_port = &port->state->port;
+	unsigned char ch, flags;
+	unsigned short status;
 
+	while((status = nordic_uart_readl(port, NORDIC_UART_STATUS_REG)) &
+			NORDIC_UART_STATUS_RRDY_MSK) {
+		ch = nordic_uart_readl(port, NORDIC_UART_RXDATA_REG);
+		flags = TTY_NORMAL;
+		port->icount.rx++;
+
+		if (status & NORDIC_UART_STATUS_E_MASK) {
+			nordic_uart_writel(port, status, NORDIC_UART_STATUS_REG);
+
+			if (status & NORDIC_UART_STATUS_BRK_MSK) {
+				port->icount.brk++;
+				if (uart_handle_break(port))
+					continue;
+			} else if (status & NORDIC_UART_STATUS_PE_MSK) {
+				port->icount.parity++;
+			} else if (status & NORDIC_UART_STATUS_ROE_MSK) {
+				port->icount.overrun++;
+			} else if (status & NORDIC_UART_STATUS_FE_MSK) {
+				port->icount.frame++;
+			}
+
+			status &= port->read_status_mask;
+
+			if (status & NORDIC_UART_STATUS_BRK_MSK)
+				flags = TTY_BREAK;
+			else if (status & NORDIC_UART_STATUS_PE_MSK)
+				flags = TTY_PARITY;
+			else if (status & NORDIC_UART_STATUS_FE_MSK)
+				flags = TTY_FRAME;
+		}
+		if (uart_handle_sysrq_char(port, ch))
+				continue;
+
+			uart_insert_char(port, status, NORDIC_UART_STATUS_ROE_MSK, ch, flags);
+	}
+
+	tty_flip_buffer_push(tty_port);
 }
 
 static void nordic_uart_tx_chars(struct uart_port *port)
 {
+	u8 ch;
 
+	uart_port_tx(port, ch,
+		nordic_uart_readl(port, NORDIC_UART_STATUS_REG) &
+						NORDIC_UART_STATUS_TRDY_MSK,
+		nordic_uart_writel(port, ch, NORDIC_UART_TXDATA_REG));
 }
 
 static irqreturn_t nordic_uart_interrupt(int irq, void *data)
 {
+	struct uart_port *port = data;
+	unsigned int isr;
 
+	isr = nordic_uart_readl(port, NORDIC_UART_STATUS_REG);
+	spin_lock_irqsave(&port->lock, flags);
+	if (isr & NORDIC_RX_INIT_STAT)
+		nordic_uart_rx_chars(port);
+
+	if (isr & NORDIC_TX_INIT_STAT);
+		nordic_uart_tx_chars(port);
+
+	spin_unlock_irqrestore(&port->lock, flags);
+
+	return IRQ_RETVAL(isr);
 }
 
 static void nordic_uart_timer(struct timer_list *pt)
@@ -141,22 +200,55 @@ static void nordic_uart_config_port(struct uart_port *port, int flags)
 
 static int nordic_uart_startup(struct uart_port *port)
 {
-	struct nordic_uart *pp = container_of(port, struct nordic_uart, port);
+	// struct nordic_uart *pp = container_of(port, struct nordic_uart, port);
 	unsigned long flags;
+	int ret = -1;
 
+	/* mask all irq and flush port */
+	nordic_uart_disable(port);
+	nordic_uart_writel()
 	if (!port->irq) {
-		timer_setup()
+		//timer_setup()
+		pr_err(DRV_NAME ": UART does not support irq. 
+			You can try modify source code to support timer polling for this driver!!!\n")
+		return ret;
 	} else {
-		int ret;
-
 		ret = request_irq(port->irq, nordic_uart_interrupt, 0,
 						DRV_NAME, port);
+		if (ret) {
+			pr_err(DRV_NAME ": unable to attach Nordic UART %d "
+					"interrupt vector=%d\n", port->line, port->irq);
+			return ret;
+		}
 	}
+
+	spin_lock_irqsave(&port->lock, flags);
+
+	/* Enable RX interrupt now */
+	nordic_uart_writel();
+
+	spin_unlock_irqrestore(&port->lock, flags);
+	nordic_uart_enable(port);
+
+	return 0;
 }
 
+/*
+ * serial core request to flush and disable uart
+ */
 static void nordic_uart_shutdown(struct uart_port *port)
 {
+	unsigned long flags;
 
+	spin_lock_irqsave(&port->lock, flags);
+
+	/* Disable interrupt now */
+	nordic_uart_writel(port, 0, UART_IR_REG);
+	spin_unlock_irqrestore(&port->irq, flags);
+
+	nordic_uart_disable(port);
+	nordic_uart_flush(port);
+	free_irq(port->irq, port);
 }
 
 nordic_uart_tx_empty,
@@ -216,21 +308,59 @@ static int nordic_uart_probe(struct platform_device *pdev)
 	struct nordic_uart_platform_uart *pln = dev_get_platdata(&pdev->dev);
 	struct uart_port *port;
 	struct resource *res_mem;
-	int i = pdev->id;
 	int ret;
 
-	/* if id is -1 scan for a free id and use that one */
-	if (i == -1) {
-		for (i = 0; i < CONFIG_SERIAL_NORDIC_UART_MAXPORTS; i++) {
-			if (nordic_uart_ports[i].part.mapbase == 0)
-				break;
-		}
+
+	if (pdev->dev.of_node) {
+		pdev->id = of_alias_get_id(pdev->dev.of_node, "serial");
+		if (pdev->id < 0)
+			pdev->id = of_alias_get_id(pdev->dev.of_node, "uart");
 	}
+	
+	if (pdev->id < 0 || pdev->id >= CONFIG_SERIAL_NORDIC_UART_MAXPORTS)
+		return -EINVAL; 
+
+	port = &nordic_uart_ports[pdev->id];
+	if (port->membase)
+		return -EBUSY;
+	memset(port, 0, sizeof(*port));
+
+	res_mem = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+	if (!res_mem)
+		return -ENODEV;
+
+	port->mapbase = res_mem->start;
+	port->membase = devm_ioremap_resource(&pdev->dev, res_mem);
+	if (IS_ERR(port->membase))
+		return PTR_ERR(port->membase);
 
 	ret = platform_get_irq(pdev, 0);
 	if (ret < 0)
 		return ret;
 	port->irq = ret;
+
+	// clk = clk_get(&pdev->dev, "refclk");
+	// if (IS_ERR(clk) && pdev->dev.of_node)
+	// 	clk = of_clk_get(pdev->dev.of_node, 0);
+
+	port->iotype = UPIO_MEM;
+	port->ops = &nordic_uart_ops;
+	port->flags = UPF_BOOT_AUTOCONF;
+	port->dev = &pdev->dev;
+	port->fifosize = 16;
+	//port->uartclk = clk_get_rate(clk) / 2;
+	port->line = pdev->id;
+	//port->has_sysrq = IS_ENABLE(CONFIG_SERIAL_NORDIC_CONSOLE);
+	// clk_put(clk);
+
+	ret = uart_add_one_port(&nordic_uart_driver, port);
+	if (ret) {
+		nordic_uart_ports[pdev->id].membase = NULL;
+		return ret;
+	}
+	platform_set_drvdata(pdev, port);
+
+	return 0;
 }
 
 static int nordic_uart_remove(struct platform_device *pdev)
